78ae164c0e66c8a2b3a1bd020ac9da38578fc355
Fandy Ahmad <fandy.ahmad@tiket.com>
2019-03-04 18:02:37 +0700

    git add .

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const DB_ADDR = process.env.MONGO_DB_ADDR || 'mongo';
 const DB_PORT = process.env.MONGO_DB_PORT || 27017;
 const DB_NAME = process.env.MONGO_DATABASE || 'api'
 const DB_CONNECTION = 'mongodb://' + DB_ADDR + ':' + DB_PORT + '/' + DB_NAME;
-
 const options = {
     useNewUrlParser: true,
     reconnectTries: 60,
@@ -23,8 +25,6 @@ app.use('/api', routes)
 app.use((request, response) => {
     response.status(404)
 })
-
-
 
 MongoClient.connect(DB_CONNECTION, options, (err, database) => {
     if (err) {
